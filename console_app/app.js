0d9d384d78b69ba01eba32dd18fb9cf28bf83714
cazar27 <carloszr27@gmail.com>
2022-06-12 14:22:22 +0200

    finish with node console app to multiply

--- console_app/app.js
+++ console_app/app.js
@@ -1,22 +1,21 @@
-const argv = require('./config/yargs').argv;
-const colors = require('colors/safe');
+const argv = require("./config/yargs").argv;
+const colors = require("colors/safe");
 
-const { makeFile, listTable } = require('./helpers/multiply');
+const { makeFile, listTable } = require("./helpers/multiply");
 
 let comando = argv._[0];
 
 switch (comando) {
+  case "list":
+    listTable(argv.base, argv.limit);
+    break;
 
-    case 'list':
-        listTable(argv.base, argv.limit);
-        break;
+  case "make":
+    makeFile(argv.base, argv.limit)
+      .then((file) => console.log(`Archivo creado: `, colors.green(file)))
+      .catch((e) => console.log(e));
+    break;
 
-    case 'make':
-        makeFile(argv.base, argv.limit)
-            .then(file => console.log(`Archivo creado: `, colors.green(file)))
-            .catch(e => console.log(e));
-        break;
-
-    default:
-        console.log('Comando no reconocido');
+  default:
+    console.log("Comando no reconocido");
 }
--- console_app/config/yargs.js
+++ console_app/config/yargs.js
@@ -1,22 +1,19 @@
 const opts = {
-    base: {
-        demand: true,
-        alias: 'b'
-    },
-    limite: {
-        alias: 'l',
-        default: 10
-    }
-}
+  base: {
+    demand: true,
+    alias: "b"
+  },
+  limite: {
+    alias: "l",
+    default: 10
+  }
+};
 
-
-const argv = require('yargs')
-    .command('listar', 'Imprime en consola la tabla de multiplicar', opts)
-    .command('crear', 'Genera un archivo con la tabla de multiplicar', opts)
-    .help()
-    .argv;
-
+const argv = require("yargs")
+  .command("list", "Imprime en consola la tabla de multiplicar", opts)
+  .command("make", "Genera un archivo con la tabla de multiplicar", opts)
+  .help().argv;
 
 module.exports = {
-    argv
-}
+  argv
+};
